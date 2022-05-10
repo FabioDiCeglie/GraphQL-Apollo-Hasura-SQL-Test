5336f56e92f855339b8d876706fbcfc59985314d
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-10 16:36:05 +0200

    show tennis matches

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+
 import {
   Container,
   Typography,
@@ -36,26 +37,56 @@ function MatchList() {
   return (
     <Container>
       <Typography variant="h2">Tennis Matches</Typography>
-      <Box>
-        {dataFilter.map((match) => (
-          <div key={match.id}>
-            <TableContainer component={Paper}>
+      <hr />
+
+      {dataFilter.map((match) => (
+        <Fragment key={match.id}>
+          <Box mt={5}>
+            <TableContainer component={Paper} mt={20}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Players</TableCell>
-                    {match.setts.map((set) => (
-                      <>
+                    {match.setts.map((set, i) => (
+                      <Fragment key={i}>
                         <TableCell align="right">Set:{set.nr}</TableCell>
-                      </>
+                      </Fragment>
                     ))}
                   </TableRow>
                 </TableHead>
+                <TableBody>
+                  <TableRow
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {match.p1.name}
+                    </TableCell>
+                    {match.setts.map((set, i) => (
+                      <Fragment key={i}>
+                        <TableCell align="right">{set.p1_score}</TableCell>
+                      </Fragment>
+                    ))}
+                  </TableRow>
+                </TableBody>
+                <TableBody>
+                  <TableRow
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {match.p2.name}
+                    </TableCell>
+                    {match.setts.map((set, i) => (
+                      <Fragment key={i}>
+                        <TableCell align="right">{set.p2_score}</TableCell>
+                      </Fragment>
+                    ))}
+                  </TableRow>
+                </TableBody>
               </Table>
             </TableContainer>
-          </div>
-        ))}
-      </Box>
+          </Box>
+        </Fragment>
+      ))}
     </Container>
   );
 }
